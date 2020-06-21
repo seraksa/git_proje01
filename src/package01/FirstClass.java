853454e234c3dfbe264c10daa020a2c19791208b
Serhat Ay <seraksa18@gmail.com>
2020-06-21 21:33:28 +0200

    feature branchtan ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
+		System.out.println("yeni branch olusturdum ve adi feature");
 
 	}
 
